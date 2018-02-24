4d3c89f6e8d1e5fc47a6e0f4eb42aa9cd0f6e231
Jaydeep Solanki <jaydp17@gmail.com>
2018-02-24 20:58:58 +0530

    auto send play/pause updates to server on WS

--- src/socker.middleware.js
+++ src/socker.middleware.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
-import { OTHER_LISTENING_CHANGE } from './common.actiontypes';
+import * as commonActionTypes from './common.actiontypes';
+import * as songActionTypes from './modules/Song/song.actiontypes';
 
 const socket = io(process.env.REACT_APP_API_HOST);
 const eventName = 'listeners-change';
@@ -8,11 +9,18 @@ export default store => next => action => {
   // eslint-disable-next-line no-underscore-dangle
   if (socket._callbacks[`$${eventName}`] == null) {
     // Handler not present, install now
-    socket.on(eventName, () => {
+    socket.on(eventName, payload => {
       store.dispatch({
-        type: OTHER_LISTENING_CHANGE,
+        type: commonActionTypes.OTHER_LISTENING_CHANGE,
+        payload,
       });
     });
+  }
+
+  if (action.type === songActionTypes.PLAY_SONG_ID) {
+    socket.emit('start-listening', action.payload);
+  } else if (action.type === songActionTypes.PAUSE_SONG_ID) {
+    socket.emit('stop-listening', action.payload);
   }
 
   next(action);
