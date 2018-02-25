16e283644ffbd17089de0122d8081d783fb5f593
Jaydeep Solanki <jaydp17@gmail.com>
2018-02-25 05:52:56 +0530

    limit the width of the song list container

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 
 import SongsList from './modules/SongsList';
+import './App.css';
 
 export default () => (
   <div className="App">
     <AppBar title="Music Player" />
-    <SongsList />
+    <div className="songs-list-container">
+      <SongsList />
+    </div>
   </div>
 );
